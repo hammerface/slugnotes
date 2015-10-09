3ae0e8f78910ddacf99b354310a422d53a545574
ajanakos <ajanakos@ucsc.edu>
2015-10-09 14:42:35 -0700

    Set Up django + SQL server
    connected django app to SQL server

--- study_notes_project/study_notes_project/settings.py
+++ study_notes_project/study_notes_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import hidden
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'study_notes_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': hidden.name,
+        'USER': hidden.user,
+        'PASSWORD': hidden.password,
+        'HOST': hidden.host,
+        'PORT': hidden.port,
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
