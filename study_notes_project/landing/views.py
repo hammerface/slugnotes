0558bc0bbb5c0b77b2e5ae77d6567cc96c133354
Evadai <evadai@ucsc.edu>
2015-10-25 11:30:04 -0700

    show decks
    show all the decks associated with a user.

--- study_notes_project/landing/views.py
+++ study_notes_project/landing/views.py
@@ -10,17 +10,30 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 def Home(request):
     form = None
+    deck_list = []
     if request.user.is_authenticated:
+        user_id = request.user.id
+        #new deck form
         if request.method == "POST":
             form = NewDeck(request.POST)
             if form.is_valid():
                 deck = form.save(commit=False)
-                deck.user_id = request.user.id
+                deck.user_id = user_id
                 deck.save()
                 return HttpResponseRedirect('/')
         else:
             form = NewDeck()
+        #grab all decks associated with a user order by descending date created
+        decks = Deck.objects.filter(user_id=user_id).order_by('-date_created')
+        for deck in decks:
+            deck_list.append({
+                            "deck_id" : deck.deck_id, 
+                            "deck_name" : deck.deck_name, 
+                            "share" : deck.share_flag
+                            })        
+
     context = {
-        "form" : form,
+        "form"      : form,
+        "deck_list" : deck_list,
     }
     return render(request, "landing/home.html", context)
