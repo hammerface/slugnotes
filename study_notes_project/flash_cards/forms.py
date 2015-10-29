3ca8d8f6dc7b4b45ef71cae1b1328f95c5a005bc
Evadai <evadai@ucsc.edu>
2015-10-29 16:44:49 -0700

    validation length quick fix
    Changed the character validation code

--- study_notes_project/flash_cards/forms.py
+++ study_notes_project/flash_cards/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Deck, Card
 
 class NewDeck(forms.ModelForm):
-
 	class Meta:
 		model = Deck
 		fields = ("user", "deck_name", "share_flag")
@@ -13,9 +12,6 @@ class NewDeck(forms.ModelForm):
 	def clean_deck_name(self):
 		user_id = self.cleaned_data['user']
 		deck_name = self.cleaned_data['deck_name']
-		print deck_name
-		if len(deck_name) > 32:
-			raise forms.ValidationError('Exceeds max character length of 32.')
 		total = None
 	
 		total = Deck.objects.filter(deck_name=deck_name, user_id=user_id)
