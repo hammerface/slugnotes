d486286abbfcc4f4bf819a27d1a42da316ab27a5
Evadai <evadai@ucsc.edu>
2015-10-29 16:34:42 -0700

    Changed deck create form
    Changed the deck creation form so that you cannot have two decks with
    the same name and the max characters cannot exceed 32 characters

--- study_notes_project/flash_cards/forms.py
+++ study_notes_project/flash_cards/forms.py
@@ -5,7 +5,21 @@ class NewDeck(forms.ModelForm):
 
 	class Meta:
 		model = Deck
-		fields = ("deck_name", "share_flag")
+		fields = ("user", "deck_name", "share_flag")
 		labels = {
 			'share_flag' : 'Share with others'
 		}
+		widgets = {'user': forms.HiddenInput()}
+	def clean_deck_name(self):
+		user_id = self.cleaned_data['user']
+		deck_name = self.cleaned_data['deck_name']
+		print deck_name
+		if len(deck_name) > 32:
+			raise forms.ValidationError('Exceeds max character length of 32.')
+		total = None
+	
+		total = Deck.objects.filter(deck_name=deck_name, user_id=user_id)
+		if not total:
+			return deck_name
+		if "deck_name" in self.changed_data:
+			raise forms.ValidationError('You already have a deck with that name.')
