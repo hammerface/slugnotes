c5e1f6a397ded98217cc965af343d8a2c8c84797
Sara Woods <sarawoods@Saras-MacBook-Air.local>
2015-10-29 16:44:01 -0700

    First attempt at create new deck modal and flash card decks (not fully functional)

--- study_notes_project/flash_cards/forms.py
+++ study_notes_project/flash_cards/forms.py
@@ -2,10 +2,13 @@ from django import forms
 from .models import Deck, Card
 
 class NewDeck(forms.ModelForm):
+	deck_name = forms.CharField(required=True)
 
 	class Meta:
 		model = Deck
 		fields = ("deck_name", "share_flag")
 		labels = {
-			'share_flag' : 'Share with others'
+			'share_flag' : 'Share With Others'
 		}
+
+
