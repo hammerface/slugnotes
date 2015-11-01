e9ab94899bc868126b674b21a08fc5a00cefebb9
Jim Brower <jrbrower@laptop-debian>
2015-11-01 14:32:39 -0800

    I think the parser is done. I didn't know where to put it so feel free to move it.

---
+++ study_notes_project/flash_cards/parse_notes.py
@@ -0,0 +1,30 @@
+import re
+
+# input:  A file path
+# output: A list representing a deck of cards. Each element of the list is
+#   itself a list. The first element of an inner list is the string on the
+#   front of the card and the rest of the elements are strings of the bullet
+#   points on the back of the card.
+#   Returns an empty list in the case where there are no cards.
+def parse_notes(path):
+        f = open(path, "r")
+        deck = []        
+        front_found = False
+        curr_card = None
+        
+        for line in f:
+            if front_found:
+                match = re.search("^B:\s*(.*)$", line)
+                if match:
+                    curr_card.append(match.group(1))
+
+            match = re.search("^F:\s*(.*)$", line)
+            if match:
+                front_found = True
+                curr_card = []
+                curr_card.append(match.group(1))
+                deck.append(curr_card)
+
+        f.close()
+        return deck
+
