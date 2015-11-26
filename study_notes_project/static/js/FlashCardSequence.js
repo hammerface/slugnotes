57fe193744070d4e01c5fd3b9bbe4907dd0be307
Sara Woods <samwoods@ucsc.edu>
2015-11-25 17:07:01 -0800

    Cleaned up javascript

--- study_notes_project/static/js/FlashCardSequence.js
+++ study_notes_project/static/js/FlashCardSequence.js
@@ -14,6 +14,8 @@ var options = {
 if(sequenceElement != null){
     // Launch Sequence on the element, and with the options we specified above
     var mySequence = sequence(sequenceElement, options);
+    // enable scrolling for flashcard if necessary
+    setTimeout(set_scroll, 300)
 }
 
 $('div.fcseq_flip').click(function(){
@@ -25,27 +27,14 @@ $('div.fcseq_flip').click(function(){
     var containing_div = $('.seq')
     containing_div.toggleClass('fcseq_card_front');
     containing_div.toggleClass('fcseq_card_back');
-
-    // scroll back to top
-    $('#sequence').scrollTop(0);
-    var new_card_content = $('.seq-in');
-    // if scroll height is bigger than the div height, turn on scrolling
-    if(new_card_content.outerHeight() < new_card_content[0].scrollHeight){
-        $('#sequence').css("overflow-y", "auto");
-    // otherwise, turn of scrolling
-    } else {
-        $('#sequence').css("overflow-y", "hidden");   
-    }
+    // enable scrolling for flashcard if necessary
+    set_scroll();
 });
 
 $('div.fcseq_next').click(function(){
     var card_content = $('.seq-in');
-
     var numcards = mySequence.$steps.length;
     var currentcardnum = mySequence.currentStepId;
-
-    console.log("currentcardnum: " + currentcardnum);
-
     // if current card is 1 and clicking next button, 
     // make previous button visible
     if(1 == currentcardnum){
@@ -59,42 +48,16 @@ $('div.fcseq_next').click(function(){
         $('a.seq-next').toggleClass('prevent-click');
     }
     // after current card is animated off screen
-    setTimeout(function(){
-        var containing_div = $('.seq')
-        // remove all front/back color classes
-        containing_div.removeClass('fcseq_card_front fcseq_card_back');
-        // add the front color class
-        containing_div.addClass('fcseq_card_front');
-
-        // remove hidden class from both front and back of the now "hidden" card 
-        card_content.find('.fcseq_card_front, .fcseq_card_back').removeClass('hidden');
-        // add hidden class to back of the card
-        card_content.find('.fcseq_card_back').addClass('hidden');
-    },400)
-
+    setTimeout(function(){set_card_to_front(card_content)}, 400)
     // after new card begins to animate on screen
-    setTimeout(function(){
-        // scroll back to top
-        $('#sequence').scrollTop(0);
-        var new_card_content = $('.seq-in');
-        // if scroll height is bigger than the div height, turn on scrolling
-        if(new_card_content.outerHeight() < new_card_content[0].scrollHeight){
-            $('#sequence').css("overflow-y", "auto");
-        // otherwise, turn of scrolling
-        } else {
-            $('#sequence').css("overflow-y", "hidden");   
-        }
-    }, 500)
+    setTimeout(set_scroll, 500)
 
 });
 
 $('div.fcseq_prev').click(function(){
     var card_content = $('.seq-in');
-
     var numcards = mySequence.$steps.length;
     var currentcardnum = mySequence.currentStepId;
-    console.log(currentcardnum);
-
     // if current card is last and clicking previous button,
     // toggle no click on next
     if(numcards == currentcardnum){
@@ -108,33 +71,9 @@ $('div.fcseq_prev').click(function(){
         $('a.seq-prev').toggleClass('prevent-click');
     }
     // after current card is animated off screen
-    setTimeout(function(){
-        var containing_div = $('.seq')
-        // remove all front/back color classes
-        containing_div.removeClass('fcseq_card_front fcseq_card_back');
-        // add the front color class
-        containing_div.addClass('fcseq_card_front');
-
-        // remove hidden class from both front and back of the now "hidden" card 
-        card_content.find('.fcseq_card_front, .fcseq_card_back').removeClass('hidden');
-        // add hidden class to back of the card
-        card_content.find('.fcseq_card_back').addClass('hidden');
-
-    }, 400)
-
+    setTimeout(function(){set_card_to_front(card_content)}, 400)
     // after new card begins to animate on screen
-    setTimeout(function(){
-        // scroll back to top
-        $('#sequence').scrollTop(0);
-        var new_card_content = $('.seq-in');
-        // if scroll height is bigger than the div height, turn on scrolling
-        if(new_card_content.outerHeight() < new_card_content[0].scrollHeight){
-            $('#sequence').css("overflow-y", "auto");
-        // otherwise, turn of scrolling
-        } else {
-            $('#sequence').css("overflow-y", "hidden");   
-        }
-    }, 500)
+    setTimeout(set_scroll, 500)
 });
 
 $('div.fcseq_random').click(function(){
@@ -143,35 +82,47 @@ $('div.fcseq_random').click(function(){
     var ul = document.querySelector("ul.seq-canvas"), // get the list
     random_ul = ul.cloneNode(true), // clone the list
     i = random_ul.children.length + 1;
-
     // shuffle the cloned list
     while( i-- > 0 )
         random_ul.appendChild( random_ul.children[Math.random() * i |0] );
     // copy shuffled list back to DOM element
     ul.parentNode.replaceChild(random_ul, ul);
-
+    // reset card color/face
+    set_card_to_front($('.seq-in'));
     // set previous button to be hidden and unclickable
     $('a.seq-prev div').css('visibility', 'hidden');
     $('a.seq-prev').removeClass('prevent-click');
     $('a.seq-prev').addClass('prevent-click');
-
     // set the next button to be visible and clickable
     $('a.seq-next div').css('visibility', 'visible');
     $('a.seq-next').removeClass('prevent-click');
-
-    // set the color of the cards to the front-color
-    var containing_div = $('.seq');
-    containing_div.removeClass('fcseq_card_front fcseq_card_back');
-    containing_div.addClass('fcseq_card_front');
-
-    // set the current card to display it's front
-    var card_content = $('.seq-in');
-    card_content.find('.fcseq_card_front, .fcseq_card_back').removeClass('hidden');
-    card_content.find('.fcseq_card_back').addClass('hidden');
-
     // destroy the current sequence and make a new one with the newly randomized cards
     mySequence.destroy();
     mySequence = sequence(sequenceElement, options);
+    // enable scrolling for flashcard if necessary
+    setTimeout(set_scroll, 300)
 
 });
 
+function set_scroll(){
+        // scroll back to top
+        $('#sequence').scrollTop(0);
+        var new_card_content = $('.seq-in');
+        // if content is too long to fit in the div, turn on scrolling
+        if(new_card_content.outerHeight() < new_card_content[0].scrollHeight){
+            $('#sequence').css("overflow-y", "auto");
+        // otherwise, turn of scrolling
+        } else {
+            $('#sequence').css("overflow-y", "hidden");   
+        }
+}
+
+function set_card_to_front(card_content){
+        // set the color of the cards to the front-color
+        var containing_div = $('.seq')
+        containing_div.removeClass('fcseq_card_front fcseq_card_back');
+        containing_div.addClass('fcseq_card_front');
+        // set the now hidden card to display it's front
+        card_content.find('.fcseq_card_front, .fcseq_card_back').removeClass('hidden');
+        card_content.find('.fcseq_card_back').addClass('hidden');
+}
